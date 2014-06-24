d94842b655e48ba14fd1a84783adb5e532349433
Buddy Brewer <buddy@buddybrewer.com>
2014-06-24 12:01:50 -0700

    Add JS examples for resource timing.

---
+++ src/object-collection.js
@@ -0,0 +1,10 @@
+var i, first, last, entries = performance.getEntries();
+for (i=0; i<entries.length; i++) {
+  if (entries[i].name.indexOf('platform.twitter.com') != -1) {
+    if (first === undefined) first = entries[i];
+    if (last === undefined) last = entries[i];
+    if (entries[i].startTime < first.startTime) first = entries[i];
+    if (entries[i].responseEnd > last.responseEnd) last = entries[i];
+  }
+}
+console.log('Took ' + (last.responseEnd - first.startTime) + ' ms');
